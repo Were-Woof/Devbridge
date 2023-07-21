9cbb1e42d9b4dcbd08834718f80ebdf54f05a73f
Were_Woof <118316858+Were-Woof@users.noreply.github.com>
2023-07-21 23:54:23 +0800

    Update hypixelApi.js

--- src/util/hypixelApi.js
+++ src/util/hypixelApi.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
-    name: "util:HypixelApi"
+    async getBedwarsStats(player) {
+        fetch('https://sessionserver.mojang.com/session/minecraft/profile/${player}').json()
+			.then(data =>{
+                let bedwarsStats = data["player"]["stats"]["Bedwars"];
+                let bedwarsWLR = (bedwarsStats['wins_bedwars'] / bedwarsStats['losses_bedwars']);
+                let bedwarsFKDR = (bedwarsStats['final_kills_bedwars'] / bedwarsStats['final_deaths_bedwars]);
+                let payload = {
+                	wlr: bedwarsWLR,
+                	fkdr: bedwarsFKDR
+                }
+            })
+			.catch(error => console.log(error))   
 }
